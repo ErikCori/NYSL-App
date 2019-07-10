46a1a4bc8a49406ec3452d387f3a517c9756c7b6
aibbya <aibbyale@gmail.com>
2019-07-10 12:15:15 -0300

    cambios en la version url

--- public/script/main.js
+++ public/script/main.js
@@ -1,12 +1,13 @@
-var partidos = data.partidos;
+var partidos = app.partidos;
 var meses = partidos.map(partido => partido.month);
 
 meses = meses.filter((mes, i, array) => array.indexOf(mes) === i);
 meses.sort();
 
 listaDropdownMonths(meses);
-
 filtrarYMostrarTabla();
+
+
 
 //********************Funciones************************
 function filtrarYMostrarTabla() {
@@ -15,7 +16,11 @@ function filtrarYMostrarTabla() {
 	if (mesSelec != "") {
 		partidosFiltrados = partidos.filter(partido => partido.month == mesSelec);
 	}
-	crearTablaEnhtml(partidosFiltrados, mesSelec);
+	else{
+		partidosFiltrados = app.partidos;
+	}
+	
+	app.partidos = partidosFiltrados;
 }
 
 function listaDropdownMonths(meses) {
@@ -31,30 +36,3 @@ function listaDropdownMonths(meses) {
 	})
 }
 
-function crearTablaEnhtml(lista, elemento) {
-	var tabla = document.getElementById('table-data');
-	tabla.innerHTML = "";
-	var contenidoTabla = crearContenidoTabla(lista, elemento);
-	tabla.innerHTML = contenidoTabla;
-}
-
-function crearContenidoTabla(partidos, elemento) {
-	var table = '<thead class="thead"><tr><th>Date</th><th>Teams</th><th>Location</th><th>Times</th></tr></thead>';
-
-	table += '<tbody>';
-
-	partidos.forEach(function (partido) {
-		table += '<tr>';
-		table += '<td class="date">' + partido.date + '</td>';
-		table += '<td class="state">' + partido.teams + '</td>';
-		table += 
-			'<td class="location"><a href=' +partido["url-location"]+ '>' + partido.location + 
-			
-			'</td>';
-		table += '<td class="times">' + partido.times + '</td>';
-		table += '</tr>';
-	})
-	table += '</tbody>';
-	return table;
-}
-	
--- public/script/mainVue.js
+++ public/script/mainVue.js
@@ -1,10 +1,31 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        section: "home",
-    }
+Vue.component('partido', {
+	props: ['date', 'times', 'teams', 'location', 'url'],
+	template: `
+		<tr>
+			<td>{{date}} {{times}}</td>
+			<td>{{teams}}</td>	
+			<td><a v-bind:href= "url">{{location}}</a></td>
+		</tr>
+	`,
 })
 
-function cambiarSection(nuevaSeccion){
-  app.section = nuevaSeccion;
+var app = new Vue({
+	el: '#app',
+	data: {
+		section: "home",
+		partidos: [],
+	},
+	methods: {
+		selectPlace(url, index) {
+			$("#map-selected").attr("src", url);
+			$(".btn-place-class").removeClass("actualClass");
+			$("#btn-place" + index).addClass("actualClass");
+		}
+	}
+})
+
+app.partidos = data.partidos;
+
+function cambiarSection(nuevaSeccion) {
+	app.section = nuevaSeccion;
 }
